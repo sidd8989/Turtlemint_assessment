426e2c7326f59372ff81e610ade139adfd9bc58b
Siddharth Jain <siddharthjain2121@gmail.com>
2024-05-29 20:09:51 +0530

    Dummy test commit

--- Turtlemint_assessment1/src/main/java/org/example/com.turtlemint.assessment/controller/product.java
+++ Turtlemint_assessment1/src/main/java/org/example/com.turtlemint.assessment/controller/product.java
@@ -2,45 +2,7 @@ package org.example.com.turtlemint.assessment.controller;
 
 public class product {
 }
-package com.infosys.SpringBootCRUD.controller;
-
-import com.infosys.SpringBootCRUD.entity.Product;
-import com.infosys.SpringBootCRUD.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-        import java.util.List;
-
-@RestController
-public class ProductController {
-    @Autowired
-    private ProductService service;
-    @PostMapping("/addProduct")
-    public Product addProduct(@RequestBody Product product){
-        return service.saveProduct(product);
-    }
-    @PostMapping("/addProducts")
-    public List<Product> addProducts(@RequestBody List<Product> products){
-        return service.saveProducts(products);
-    }
-    @GetMapping("/products")
-    public List<Product> findAllProducts(){
-        return service.getProducts();
-    }
-    @GetMapping("/product/{id}")
-    public  Product findProductById(@PathVariable int id){
-        return service.getProductById(id);
-    }
-    @PostMapping("/update")
-    public Product updateProduct(@RequestBody Product product){
-        return service.updateProduct(product);
-
-    }
-    @DeleteMapping("/delete/{id}")
-    public String  deleteProduct(@PathVariable int id){
-        return service.deleteProduct(id);
-    }
 
 
-}
+
 
